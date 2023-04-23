0342884f074ec05367112557057e820e81132070
Beket <dattebayokz@gmail.com>
2023-04-23 12:38:55 +0600

    updating the game and README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+This is the "Rock, Paper, Scissors War" game for the Nfactorial Incubator 2023 test task. 
+The game is based on popular worldwide Rock, Paper, Scissors but in the new format
+
+To run the game you need to install python version 3.9+ and pygame 2.3+. In order to correctly visualize all elements of the game, you need to download all the assets into the assets folder.
+
+After installing run in the console command "python3 main.py". This will open game window with welcome screen. By pressing the start button(it may take several tries) you will be forwarded to the selection screen where you can predict who will win the war. After that epic battle will begin. 
+To pause the game press "p" button. To quit the game click on the quit button on the top left or right of the window.
+
+Have fun!!!
--- main.py
+++ main.py
@@ -11,6 +11,11 @@ RUNNING = True
 ALL_SAME = False
 IS_PAUSED = False
 IS_MUSIC_ON = True
+WELCOME_STATE = True
+SELECTION_STATE = False
+MAIN_STATE = False
+END_STATE = False
+WINNER_SELECTION = ""
 
 images = {
     "rock": "./assets/rock.png",
@@ -23,7 +28,10 @@ total_num = 100
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Rock, Paper, Scissor War")
 clock = pygame.time.Clock()
+font = pygame.font.SysFont('comicsansms', 100)
+
 
 def generate_elements(num_elements, element_class, all_elements_group):
     element_group = pygame.sprite.Group()
@@ -52,9 +60,6 @@ class GameObject(pygame.sprite.Sprite):
     def draw(self):
         screen.blit(self.image, self.rect)
 
-
-    def update(self ):
-        pass
 
     def move(self):
         self.detect_collision_with_walls()
@@ -108,7 +113,7 @@ class GameObject(pygame.sprite.Sprite):
 
 
 class Rock(GameObject):
-    def __init__(self, direction = None, pos = None):
+    def __init__(self):
         super().__init__(prey = Scissor, enemy = Paper, type = 'Rock',
         image = pygame.image.load(images["rock"]))
 
@@ -118,7 +123,7 @@ class Rock(GameObject):
 
 
 class Paper(GameObject):
-    def __init__(self, direction = None, pos = None):
+    def __init__(self):
         super().__init__(prey = Rock, enemy = Scissor, type='Paper',
         image = pygame.image.load(images["paper"]))
 
@@ -128,12 +133,23 @@ class Paper(GameObject):
 
 
 class Scissor(GameObject):
-    def __init__(self, direction = None, pos = None):
+    def __init__(self):
         super().__init__(prey = Paper, enemy = Rock, type='Scissor',
         image = pygame.image.load(images["scissors"]))
 
     def change_icon(self):
         self.image = self.prey.image
+
+
+class StartButton:
+    def __init__(self):
+        # importing brush png image for button
+        self.image = pygame.transform.scale(pygame.image.load('./assets/start_button.png'), (180, 100))
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT * 2 // 3)
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
 
     
 class GameLogic():
@@ -145,6 +161,7 @@ class GameLogic():
         self.rocks_group, self.all_sprites_group = generate_elements(self.num_rocks, Rock, self.all_sprites_group)
         self.papers_group, self.all_sprites_group = generate_elements(self.num_papers, Paper, self.all_sprites_group)
         self.scissors_group, self.all_sprites_group = generate_elements(self.num_scissors, Scissor, self.all_sprites_group)
+        self.welcome_bg_image = pygame.transform.scale(pygame.image.load('./assets/welcome_bg_1.png'), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
     def detect_sprites_collisions(self, sprite):
         for other_sprite in self.all_sprites_group:
@@ -165,7 +182,6 @@ class GameLogic():
 
     def pause(self):
         global IS_PAUSED, IS_MUSIC_ON
-        font = pygame.font.SysFont('comicsansms', 100)
         text_surf = font.render("""Paused""", True, (0, 0, 0))
         text_rect = text_surf.get_rect()
         text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
@@ -184,12 +200,9 @@ class GameLogic():
                         else:
                             mixer.music.unpause()
                         IS_MUSIC_ON = not IS_MUSIC_ON
-
-        screen.blit(text_surf, text_rect)
-        
-
-        pygame.display.flip()
-        clock.tick(FPS)  
+            screen.blit(text_surf, text_rect)
+            pygame.display.flip()
+            clock.tick(FPS)  
 
 
     def check_type(self):
@@ -198,16 +211,99 @@ class GameLogic():
             is_type_same = all(sprite.type == curr_type for sprite in self.all_sprites_group)
             return is_type_same
         
-    def draw_win_screen(self):
-                
+    def draw_welcome_screen(self):
+        global WELCOME_STATE, SELECTION_STATE
+        start_button = StartButton()
 
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_button.rect.collidepoint(pygame.mouse.get_pos()):
+                    WELCOME_STATE = False
+                    SELECTION_STATE = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_KP_ENTER:
+                    WELCOME_STATE = False
+                    SELECTION_STATE = True
+        font1 = pygame.font.SysFont('comicsansms', 50)
+        text_surf = font1.render("""Rock, Paper, Scissors War""", True, (0, 0, 0))
+        text_rect = text_surf.get_rect()
+        text_rect.center = (SCREEN_WIDTH // 2, 20)
+
+        screen.blit(self.welcome_bg_image, (0, 0))
+        screen.blit(text_surf, text_rect)
+        start_button.draw()
+
+
+    def draw_selection_screen(self):
+        global SELECTION_STATE, MAIN_STATE, WINNER_SELECTION
+        rock_select_button = pygame.transform.scale(pygame.image.load('./assets/rock_select.png'), (100, 100))
+        rock_select_rect = rock_select_button.get_rect()
+        rock_select_rect.center = (SCREEN_WIDTH // 3, SCREEN_HEIGHT // 2)
+
+        paper_select_button = pygame.transform.scale(pygame.image.load('./assets/paper_select.png'), (100, 100))
+        paper_select_rect = paper_select_button.get_rect()
+        paper_select_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        scissors_select_button = pygame.transform.scale(pygame.image.load('./assets/scissors_select.png'), (100, 100))
+        scissors_select_rect = scissors_select_button.get_rect()
+        scissors_select_rect.center = ( 3 * SCREEN_WIDTH // 4, SCREEN_HEIGHT // 2)
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if rock_select_rect.collidepoint(pygame.mouse.get_pos()):
+                    WINNER_SELECTION = "Rock"
+                    SELECTION_STATE = False
+                    MAIN_STATE = True
+                if paper_select_rect.collidepoint(pygame.mouse.get_pos()):
+                    WINNER_SELECTION = "Paper"
+                    SELECTION_STATE = False
+                    MAIN_STATE = True
+                if scissors_select_rect.collidepoint(pygame.mouse.get_pos()):
+                    WINNER_SELECTION = "Scissors"
+                    SELECTION_STATE = False
+                    MAIN_STATE = True
+        
+        font1 = pygame.font.SysFont('comicsansms', 50)
+        text_surf = font1.render("""Choose your fighter""", True, (0, 0, 0))
+        text_rect = text_surf.get_rect()
+        text_rect.center = (SCREEN_WIDTH // 2, 50)
+
+        screen.blit(text_surf, text_rect)
+        screen.blit(rock_select_button, rock_select_rect)
+        screen.blit(paper_select_button, paper_select_rect)
+        screen.blit(scissors_select_button, scissors_select_rect)
+
+
+    def draw_end_screen(self):
+        curr_type = ""
+        for sprite in self.all_sprites_group:
+            curr_type = sprite.type
+        is_right = curr_type == WINNER_SELECTION
+        
+        winner_image = pygame.transform.scale(pygame.image.load(f'./assets/{curr_type.lower()}_select.png'), (200, 200))
+        winner_rect = winner_image.get_rect()
+        winner_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        font1 = pygame.font.SysFont('comicsansms', 50)
+        text_surf1 = font1.render("""is the winner""", True, (0, 0, 0))
+        text_surf2 = font1.render("You are right", True, (0, 0, 0)) if is_right else font1.render("You are wrong", True, (0, 0, 0))
+        text_rect1 = text_surf1.get_rect()
+        text_rect2 = text_surf2.get_rect()
+        text_rect1.center = (SCREEN_WIDTH // 2, 550)
+        text_rect2.center = (SCREEN_WIDTH // 2, 50)
+
+        screen.blit(winner_image, winner_rect)
+        screen.blit(text_surf1, text_rect1)
+        screen.blit(text_surf2, text_rect2)
+
+
+        
                     
             
 
 
 
 def main():
-    global RUNNING, ALL_SAME, IS_PAUSED, IS_MUSIC_ON
+    global RUNNING, ALL_SAME, IS_PAUSED, IS_MUSIC_ON, MAIN_STATE, END_STATE
     game_logic = GameLogic()
 
     mixer.music.load('./assets/epic_music.mp3')
@@ -230,11 +326,18 @@ def main():
                     game_logic.pause()
         screen.fill((255, 255, 255))
 
-        if game_logic.check_type():
-            ALL_SAME = True
-            RUNNING = False
-
-        game_logic.update()
+        if WELCOME_STATE:
+            game_logic.draw_welcome_screen()
+        elif SELECTION_STATE:
+            game_logic.draw_selection_screen()
+        elif MAIN_STATE:
+            game_logic.update()
+            if game_logic.check_type():
+                ALL_SAME = True
+                END_STATE = True
+                MAIN_STATE = False
+        else:
+            game_logic.draw_end_screen()
 
         pygame.display.flip()
         clock.tick(60)
